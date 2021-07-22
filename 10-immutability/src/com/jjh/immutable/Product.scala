2b1b0b12c521328228a43402f2be6fa743760a0e
John Hunt <john.hunt10@gmail.com>
2021-07-22 11:39:13 +0100

    Added immutability examples

---
+++ 10-immutability/src/com/jjh/immutable/Product.scala
@@ -0,0 +1,36 @@
+package com.jjh.immutable
+
+trait Immutable
+
+trait Product extends Immutable {
+  val price: Double
+}
+
+case class Version(number: Int) extends Immutable {
+  override def toString: String = "v" + number
+
+  def next: Version = Version(number + 1)
+}
+
+case class Invoice[T <: Product](id: Int,
+                                 name: String,
+                                 contents: List[T],
+                                 version: Version = Version(0)) extends Immutable {
+  def total: Double = contents.map(_.price).sum
+
+  def +(p: T): Invoice[T] = copy(contents = p :: contents, version = version.next)
+
+  def -(p: T): Invoice[T] = copy(contents = contents diff List(p), version = version.next)
+}
+
+case class Book(title: String,
+                author: String,
+                price: Double) extends Product
+
+object Test extends App {
+  val books = List(Book("Java", "John", 12.99))
+  var i = Invoice(1, "Denise", books)
+  println(i)
+  i = i + Book("Scala", "Adam", 10.99)
+  println(i)
+}
