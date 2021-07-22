dc35d8099568d6819953ceebeef5385318283f0b
John Hunt <john.hunt10@gmail.com>
2021-07-22 11:40:24 +0100

    functional exception handling examples

---
+++ 11-func-exceptions/src/com.jjh/functional/TrySampleApp3.scala
@@ -0,0 +1,37 @@
+package com.jjh.functional
+
+import scala.util.{Failure, Success, Try}
+
+object TrySampleApp3 extends App {
+
+  val i = Try(
+    "4".toInt
+  ).map(n => n * n)
+  println(i)
+
+  // Recover takes a PartialFunction that only handles
+  // Exceptions
+  val j = Try {
+    "s".toInt
+  }.recover {
+    case nfe: NumberFormatException =>
+      println(nfe)
+      0
+    case e: Exception =>
+      println(e)
+      -1
+  }.get
+  println(j)
+
+  // Can swap the exception for something else
+  Try {
+    "s".toInt
+  }.recoverWith {
+    case nfe: NumberFormatException =>
+      println(nfe)
+      Failure(new IllegalArgumentException())
+  } match {
+    case Success(value) => println(value)
+    case Failure(exception) => println(exception)
+  }
+}
